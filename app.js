cc20feddeaad49c63d2c332c24c60993af36af41
salexzee <samwebb@codingtestsite.com>
2016-05-23 03:24:02 -0700

    mass progress

--- app.js
+++ app.js
@@ -15,45 +15,27 @@ const Question = require('./db/db.js').Question
 const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
-
-
-if (false){
-  let question1 = new Question({
-    question: 'What color is grass?',
-    answer: {
-      text: 'Green',
-      upvotes: 8,
-      downvotes: 3
-    },
-    answered: true
-  })
-
-  question1.save((err, question1) => {
-    if (err) return console.error(err)
-  })
-}
-
-Question.find((err, questions) => {
-  console.log(questions)
-})
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', routes);
-app.use('/post', routes);
+app.use('/', routes)
+app.use('/post', routes)
+app.use('/questions', routes)
+app.use('/question/:qId', routes)
+app.use('/question/:qId/post', routes)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Not Found')
   err.status = 404;
   next(err);
 });
@@ -64,23 +46,23 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
 
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const Question = require('../db/db.js').Question
 const dbHelper = require('../db/db_helpers.js')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ask and Answer' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Ask and Answer' })
 });
 
 /* Creates new question and redirects to that question page */
@@ -20,8 +20,45 @@ router.post('/post', (req, res, next) => {
     downvotes: 0,
     qId: dbHelper.randomId()
   })
-  console.log(question.question)
+
+  question.save((err, question) => {
+    if (err) return console.error(err)
+    console.log(question)
+  })
+
   res.render('index')
+})
+
+// Updates the answer for a question on POST /question/:qId/post
+router.post('/question/:qId/post', (req, res, next) => {
+  let question = Question.findOne({
+    'qId': req.params.qId
+  })
+  if (question.answered === true) {
+    res.redirect('/')
+    return
+  }
+})
+
+// Renders a specific question on GET /question/:qId
+router.get('/question/:qId', (req, res, next) => {
+  let question = Question.findOne({
+    'qId': req.params.qId
+  })
+  res.render('question', {
+    question: question
+  })
+})
+
+// Renders all questions GET /questions
+router.get('/questions', (req, res, next) => {
+  Question.find({ answered: false })
+    .sort({ upvotes: 1 }).exec((err, questions) => {
+    console.log(questions)
+    res.render('questions', {
+      questions: questions
+    })
+  })
 })
 
 module.exports = router;
